ac8c8175f642f8fbb984a1c3ff0b5f1ec82dedd8
Zyad_Mohamed296 <142996249+zezoo296@users.noreply.github.com>
2024-03-04 19:59:24 +0200

    Update CS112_A1_T6_20230159.py

--- CS112_A1_T6_20230159.py
+++ CS112_A1_T6_20230159.py
@@ -1,4 +1,4 @@
-# Program:This is the second game (Tic-Tac-Toe with numbers):A board of 3 x 3 is displayed 
+# Program:(Tic-Tac-Toe with numbers) A board of 3 x 3 is displayed 
 # and player 1 takes odd numbers 1,3, 5, 7, 9 and player 2 takes even numbers 0, 2, 4, 6, 8.
 # Players take turns to write their numbers. Odd numbers start. Use each number only once.
 # The first person to complete a line that adds up to 15 is the winner.
